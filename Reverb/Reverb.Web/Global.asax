556a8ac46628977aeaeba41a76f59f8a31071c57
Xenoleth <petar.yerzhabek@abv.bg>
2017-10-11 19:24:20 +0300

    It's alive. It's ALIVE!

---
+++ Reverb/Reverb.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Reverb.Web.MvcApplication" Language="C#" %>
---
+++ Reverb/Reverb.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Reverb.Web.Startup))]
+namespace Reverb.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

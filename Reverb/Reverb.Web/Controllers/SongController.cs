57fb3805163921135dce923e83f6b8b80b892188
Xenoleth <petar.yerzhabek@abv.bg>
2017-10-14 18:01:29 +0300

    Add search for songs

--- Reverb/Reverb.Web/Controllers/SongController.cs
+++ Reverb/Reverb.Web/Controllers/SongController.cs
@@ -30,5 +30,51 @@ namespace Reverb.Web.Controllers
 
             return View(songs);
         }
+
+        [HttpPost]
+        public ActionResult SearchSong(SongSearchViewModel songRequest)
+        {
+            var songs = this.songService
+                .GetSongs();
+
+            switch (songRequest.SearchBy)
+            {
+                case "Title":
+                    songs = songs.Where(x => x.Title.Contains(songRequest.SearchTerm));
+                    break;
+                case "Album":
+                    songs = songs.Where(x => x.Album.Title.Contains(songRequest.SearchTerm));
+                    break;
+                case "Artist":
+                    songs = songs.Where(x => x.Artist.Name.Contains(songRequest.SearchTerm));
+                    break;
+            }
+
+            if (songRequest.IsDescending)
+            {
+                songs = songs.OrderByDescending(x => x.Artist.Name)
+                    .OrderByDescending(x => x.Album.Title)
+                    .OrderByDescending(x => x.Title);
+            }
+            else
+            {
+                songs = songs.OrderBy(x => x.Artist.Name)
+                    .OrderBy(x => x.Album.Title)
+                    .OrderBy(x => x.Title);
+            }
+
+            var data = songs
+             .Select(x => new SongViewModel
+             {
+                 Title = x.Title,
+                 Artist = x.Artist.Name,
+                 Album = x.Album.Title,
+                 Lyrics = x.Lyrics
+             })
+             .ToList();
+
+
+            return View("Library", data);
+        }
     }
 }
---
+++ Reverb/Reverb.Web/Models/Song/SongSearchViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Reverb.Web.Models.Song
+{
+    public class SongSearchViewModel
+    {
+        public string SearchTerm { get; set; }
+
+        public string SearchBy { get; set; }
+
+        public bool IsDescending { get; set; }
+    }
+}
